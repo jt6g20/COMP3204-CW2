95d7aa542b782671311403917ec3cde1625dd2c0
pn3g20 <pn3g20@soton.ac.uk>
2022-12-09 15:47:17 +0000

    Renamed project files, switched to GroupedDataset

---
+++ cw2/src/main/java/comp3204/Run3.java
@@ -0,0 +1,27 @@
+package comp3204;
+
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+
+import java.io.File;
+
+/**
+ * OpenIMAJ Hello world!
+ *
+ */
+public class Run3 {
+    public static void main( String[] args ) {
+        
+        try {
+            File file = new File(Run3.class.getClassLoader().getResource("comp3204/training.zip").toURI());
+            String path = file.getAbsolutePath();
+            GroupedDataset<String, VFSListDataset<FImage>, FImage> training = new VFSGroupDataset<>("zip:" + path, ImageUtilities.FIMAGE_READER);
+            System.out.println(training.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
