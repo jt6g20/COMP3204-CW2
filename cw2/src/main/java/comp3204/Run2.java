c564ec4cdab1fa9753c506be5bd3ee4aec43203c
Josh Tucker <jt6g20@soton.ac.uk>
2023-01-02 22:04:10 +0000

    Implemented extracting vectors from patches of an image

--- cw2/src/main/java/comp3204/Run2.java
+++ cw2/src/main/java/comp3204/Run2.java
@@ -1,10 +1,14 @@
 package comp3204;
 
+import comp3204.classifiers.KNNClassifier;
+import comp3204.classifiers.OVAClassifier;
 import comp3204.utility.Data;
 import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 
@@ -18,5 +22,18 @@ public class Run2 {
         System.out.println("Training set size: " + training.size());
         System.out.println("Training set classes:" + training.getGroups());
 
+        OVAClassifier ovaClassifier = new OVAClassifier();
+
+        GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<String, FImage>(training, 50, 0, 50);
+        GroupedDataset<String, ListDataset<FImage>, FImage> trainingSubset = splits.getTrainingDataset();
+//        GroupedDataset<String, ListDataset<FImage>, FImage> testingSubset = splits.getTestDataset();
+
+//        FImage testImage = training.get("bedroom").get(1);
+//        System.out.println(testImage.width);
+//        System.out.println(testImage.height);
+//        System.out.println(ovaClassifier.extractPatchVectors(testImage).length);
+
+        ovaClassifier.train(trainingSubset);
+//        ovaClassifier.classify(testingSubset);
     }
 }
--- cw2/src/main/java/comp3204/classifiers/OVAClassifier.java
+++ cw2/src/main/java/comp3204/classifiers/OVAClassifier.java
@@ -1,12 +1,37 @@
 package comp3204.classifiers;
 
+import org.openimaj.data.DataSource;
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.feature.FloatFV;
 import org.openimaj.image.FImage;
 import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class OVAClassifier {
     LiblinearAnnotator ova;
+
+    public float[][] extractPatchVectors(FImage image){
+
+        int patchSize = 8;
+        int increment = 4;
+
+        ArrayList<float[]> vectors = new ArrayList<>();
+
+        int xIncrements = (int) Math.floor(image.getWidth() / increment);
+        int yIncrements = (int) Math.floor(image.getHeight() / increment);
+
+        for (int y = 0; y < yIncrements; y++) {
+            for (int x = 0; x < xIncrements; x++) {
+                FImage sample = image.extractROI(x*increment,y*increment, patchSize, patchSize);
+                vectors.add(sample.getPixelVectorNative(new float[sample.getWidth() * sample.getHeight()]));
+            }
+        }
+
+        return vectors.toArray(new float[vectors.get(0).length][vectors.size()]);
+    }
 
     public void train(GroupedDataset<String, ListDataset<FImage>, FImage> training){
 //        ova = LiblinearAnnotator(extractor, LiblinearAnnotator.Mode.MULTILABEL);
