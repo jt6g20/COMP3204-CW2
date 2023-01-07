d8f92cd812970e89d7e0d2571256169cce70fc4f
ads3g20 <ads3g20@soton.ac.uk>
2023-01-07 20:46:16 +0000

    Made it so that the KNNClassifier now returns the class with the highest confidence rate for k-values > 1 instead of the whole array of classes identified

--- cw2/src/main/java/comp3204/classifiers/KNNClassifier.java
+++ cw2/src/main/java/comp3204/classifiers/KNNClassifier.java
@@ -2,13 +2,15 @@ package comp3204.classifiers;
 
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
 import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.feature.FloatFV;
 import org.openimaj.feature.FloatFVComparison;
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.ml.annotation.basic.KNNAnnotator;
+
+import java.util.ArrayList;
 
 public class KNNClassifier {
     KNNAnnotator knn;
@@ -40,39 +42,57 @@ public class KNNClassifier {
 
         knn = KNNAnnotator.create(extractor, FloatFVComparison.EUCLIDEAN,kVal);
         knn.trainMultiClass(training);
-
-        /*int counter = 0;
-        for (FImage i:training){
-
-            System.out.println("image"+counter+".jpg" + " " + knn.classify((KNNClassifier.imageResize(i))).getPredictedClasses().toString().replace("[", "").replace("]", "") + " ||| " + DisplayUtilities.display(i));
-            counter++;
-            if (counter==5){
-                break;
-            }
-        }*/
     }
 
+    //Returns an arraylist of the different classification results along with their confidence level
+    public ArrayList getClassConfidence(FImage i){
+        org.openimaj.experiment.evaluation.classification.ClassificationResult result = knn.classify((KNNClassifier.imageResize(i)));
+        ArrayList confidenceList = new ArrayList();
+        for (Object x:result.getPredictedClasses()){
+            String str = x.toString().concat(": ").concat(String.valueOf(result.getConfidence(x)));
+            confidenceList.add(str);
+        }
+        return confidenceList;
+    }
+
+    //Returns the class with the highest confidence as determined by the kNN Annotator
+    public String getHighestConfidentClass(FImage i){
+        ClassificationResult result = knn.classify((KNNClassifier.imageResize(i)));
+        //Initialises an empty string and confidence value of 0
+        String classIdentified = "";
+        double confidence = 7;
+
+        //If k in kNN = 1, then the resulting Set of predicted classes will be of size 1 and therefore it returns the class inside the Set
+        if (result.getPredictedClasses().size() == 1){
+            return result.getPredictedClasses().toString().replace("[", "").replace("]", "");
+            //When k > 1, the set has multiple classes identified with different confidence rates, so
+        }else{
+            //We loop through the set
+            for (Object x:result.getPredictedClasses()){
+                //Sets the classIdentified string and confidence double to the first element in the set when they're both untouched
+                if (classIdentified.length() == 0 && confidence==0){
+                    classIdentified = x.toString();
+                    confidence = result.getConfidence(x);
+                    //If any of the classes in the set thereafter have a greater confidence, the local vars are updated
+                }else if (result.getConfidence(x) > confidence){
+                    classIdentified = x.toString();
+                    confidence = result.getConfidence(x);
+                }
+            }
+            return classIdentified;
+        }
+    }
+
+    //Applied the trained annotator to a set of images and classifies them
     public void classify(GroupedDataset<String, ListDataset<FImage>, FImage> testing){
         int counter = 0;
         for (FImage i:testing){
-
-            System.out.println("image"+counter+".jpg" + " " + knn.classify((KNNClassifier.imageResize(i))).getPredictedClasses().toString().replace("[", "").replace("]", ""));
-            /*System.out.println("image"+counter+".jpg" + " " + knn.classify((KNNClassifier.imageResize(i))).getPredictedClasses().toString().replace("[", "").replace("]", "") + " ||| " + DisplayUtilities.display(i));
+            //(uncomment the following line and comment out the one after if you want to view all the classes with their confidence rates for each image)
+            //System.out.println("image"+counter+".jpg" + " " + getHighestConfidentClass(i) + " ---- " + getClassConfidence(i));
+            System.out.println("image"+counter+".jpg" + " " + getHighestConfidentClass(i));
             counter++;
-            if (counter==5){
-                break;
-            }*/
         }
     }
-
-    /*public void classify(VFSListDataset<FImage> testing){
-        int counter = 0;
-        for (FImage i:testing){
-            System.out.println("image"+counter+ ": " + knn.classify(i).getPredictedClasses());
-            counter++;
-        }
-    }*/
-
     public KNNAnnotator getKnn() {
         return knn;
     }
