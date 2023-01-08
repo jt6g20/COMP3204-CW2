ad327829660a34b6bcb9ba5576db72f98a4907d9
pn3g20 <pn3g20@soton.ac.uk>
2023-01-08 14:08:34 +0000

    Added javadoc comments to PHOWClassifier

--- cw2/src/main/java/comp3204/classifiers/PHOWClassifier.java
+++ cw2/src/main/java/comp3204/classifiers/PHOWClassifier.java
@@ -28,35 +28,54 @@ public class PHOWClassifier {
 
     LiblinearAnnotator<FImage, String> ann;
 
-    // Trains the classifier
+    /**
+     * Trains a linear classifier by extracting DenseSIFT features using a PHOW technique
+     * @param training The training dataset
+     */
     public void train(GroupedDataset<String, ListDataset<FImage>, FImage> training){
+        // Constructs DenseSIFT extractor object, with a step size of 5 px window size of 7 px
         DenseSIFT dsift = new DenseSIFT(5, 7);
-        final PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<FImage>(dsift, 6f, 7);
+        // Constructs a Pyramid DenseSIFT extractor using the object above, with a magnification factor of 6 (controls smoothing)
+        final PyramidDenseSIFT<FImage> pdsift = new PyramidDenseSIFT<>(dsift, 6f, 7);
 
+        // Instantiates and trains a HardAssigner using a random sample of 30 images across all groups from training data
         final HardAssigner<byte[], float[], IntFloatPair> assigner =
                 getAssigner(GroupedUniformRandomisedSampler.sample(training, 30), pdsift);
 
+        // Takes in the Pyramid DenseSIFT to construct a PHOW extractor
         FeatureExtractor<DoubleFV, FImage> extractor = new FeatureExtractor<DoubleFV, FImage>() {
+            /**
+             * Extracts DenseSIFT features from an image
+             * @param object the FImage to extract from
+             * @return A spatial histogram representing the visual word occurrences, in vector form
+             */
             @Override
             public DoubleFV extractFeature(FImage object) {
                 FImage image = object.getImage();
                 pdsift.analyseImage(image);
 
-                BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<byte[]>(assigner);
+                // Uses the HardAssigner to assign each feature to a visual word
+                BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner);
 
-                BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<byte[], SparseIntFV>(
+                // Breaks the image into 4 blocks and computes the histogram for each one
+                BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(
                         bovw, 2, 2);
 
+                // returns the resultant spatial histograms, appended and normalised
                 return spatial.aggregate(pdsift.getByteKeypoints(0.015f), image.getBounds()).normaliseFV();
             }
         };
 
+        // Instantiates a Linear classifier and trains it using our PHOW extractor
         ann = new LiblinearAnnotator<>(extractor, LiblinearAnnotator.Mode.MULTICLASS,
                 SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
-
         ann.train(training);
     }
 
+    /**
+     * Applied the trained annotator to a set of images and classifies them
+     * @param testing dataset
+     */
     public void classify(GroupedDataset<String, ListDataset<FImage>, FImage> testing) {
         int count = 0;
         for (FImage i: testing) {
@@ -65,10 +84,17 @@ public class PHOWClassifier {
         }
     }
 
-    // returns a HardAssigner used to assign SIFT features to identifiers
-    public HardAssigner<byte[],float[],IntFloatPair> getAssigner(GroupedDataset<String, ListDataset<FImage>, FImage> training, PyramidDenseSIFT pdsift){
+    /**
+     * Constructs a HardAssigner trained on the dataset to perform K-means clustering on SIFT features
+     * @param training dataset of FImages
+     * @param pdsift Pyramid DenseSIFT
+     * @return a HardAssigner Class
+     */
+    public HardAssigner<byte[],float[],IntFloatPair> getAssigner(GroupedDataset<String, ListDataset<FImage>, FImage> training, PyramidDenseSIFT<FImage> pdsift){
         // Stores list of keypoint descriptors
-        List<LocalFeatureList<ByteDSIFTKeypoint>> allkeys = new ArrayList<LocalFeatureList<ByteDSIFTKeypoint>>();
+        List<LocalFeatureList<ByteDSIFTKeypoint>> allkeys = new ArrayList<>();
+
+        // Iterates through the training
         int count = 0;
         for (FImage rec : training) {
             System.out.println("Count " + count);
@@ -80,7 +106,7 @@ public class PHOWClassifier {
             count++;
         }
 
-        // takes the first 10000 features and clusters them into 300 classes
+        // Takes the first 10000 features and clusters them into 300 classes
         if (allkeys.size() > 10000)
             allkeys = allkeys.subList(0, 10000);
 
